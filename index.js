b9b54b31babd987f51fd55870059b2dd5456f641
Mayank Sharma <76662348+mayankofficial999@users.noreply.github.com>
2021-03-20 23:07:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-var hello
+var xmlhttp = new XMLHttpRequest();
+var myObj;
+xmlhttp.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+    myObj = JSON.parse(this.responseText);
+  }
+};
+xmlhttp.open("GET", "/HackTheWorld/data.json", true);
+xmlhttp.send();
+document.getElementById('Json').innerHTML=obj[Sheet1][0].name;
